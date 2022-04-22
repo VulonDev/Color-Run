404cc19c1aa6f8c1722a13577b46b893dbb78756
Elizabeth <elmarnol@ucsc.edu>
2022-04-22 15:34:26 +0200

    trying to get the assets folder to exist in git

---
+++ assets/blank.js
@@ -0,0 +1,5 @@
+//this is a completely blank code file
+//its literally just to get the assets folder to push to git
+//because it was bothering me that assets wasnt showing up
+//once you put real art in here you can and should delete this file
+//also take it out of index.html
