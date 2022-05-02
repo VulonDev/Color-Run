6f35878bc027f612f8c01ac2dabffd33ed527ae2
aplaxco1 <aplaxco@ucsc.edu>
2022-05-01 19:06:10 -0700

    adding header to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+// Collaborators: Elizabeth Arnold, Ryan Hueckel, Ziyang Li, and Autumn Plaxco
+// Game Title: Color Run
+// Date Completed: May 1, 2022
+// Creative Tilt Justification:
+//  - In terms of technical creativity, we were proud of the way we implemented randomness in our game's design.
+//    Not only were the obstacles themselves, both their layout and their color, randomly generated, so were the
+//    locations and colors or the collectibles themselves. Such degrees of randomness to ours game's design
+//    assists in generating new challenges each time the player plays the game. In particular we were proud of
+//    how we went about randomly generating obstacles. First, we had a function which created a single obstacle 
+//    of a random color (red, green, or blue) at a specified x, y coordinate, and then implemented several different
+//    functions which used this to create different layouts of these obstacles with random colors. Lastly, we had a 
+//    function which randomly called one of these obstacle layout functions, adding furtheer depth to our game's
+//    degree of randomness.
+//  - In terms of visual/artistic creativity, we are proud of how we were able to create very simple pixel art in such
+//    a way that supported our game's mechanics and themes. Because the game revolves around changing colors to avoid
+//    obstacles (which in itself we thought was pretty unique concept to introduce into the Infinite Runner format, as 
+//    it involves chaning the playable character's state to avoid obstacles, rather than only relying upon movement 
+//    capabilities), we opted to choose a very simple visual style with a grayscale background to make the interactive 
+//    elements of the game better pop out, as the colors of these elements are essential to ours game's mechanics. Each
+//    of us do not have very much experience which art/sound within games, so we are generally just proud of how well
+//    the visuals and sound turned out in the end despite our limited experience. In particular, the way that the player
+//    sprite switches colors within the game while maintining the same animations, added some pretty good visual style.
+
+
+
 //main for infinite runner
 
 let config = {
